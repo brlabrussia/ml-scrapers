92da54de7abdea4ee766a25b33ae47c917007325
Valerii Ryndin <ryndin18@icloud.com>
2020-03-03 11:52:58 +0300

    Bookmakers spider - refactoring

--- scrapy_project/spiders/bookmakers.py
+++ scrapy_project/spiders/bookmakers.py
@@ -10,42 +10,38 @@ from scrapy_project.items import Bookmaker
 
 class BookmakersSpider(scrapy.Spider):
     name = "bookmakers"
-
-    custom_settings = {
-        "CONCURRENT_REQUESTS": 1,
-    }
-
-    has_more = True
+    allowed_domains = ["metaratings.ru"]
 
     def start_requests(self):
         date = datetime.datetime.now().strftime("%d.%m.%Y")
         hash_ = hashlib.md5(f"metaratings.ru|{date}".encode()).hexdigest()
-        limit = 100
-        offset = 0
+        params = {
+            "iblock_id": 5,
+            "hash": hash_,
+            "limit": 100,
+            "offset": 0,
+        }
+        url = self.build_url_from_params(params)
+        yield scrapy.Request(url, cb_kwargs={"params": params})
 
-        # TODO: find better way to stop iteration
-        while True:
-            if not self.has_more:
-                break
-            params = {
-                "iblock_id": 5,
-                "hash": hash_,
-                "limit": limit,
-                "offset": offset,
-            }
-            url = "https://metaratings.ru/api/"
-            url = f"{url}?{urllib.parse.urlencode(params)}"
-            yield scrapy.Request(url)
-            offset += limit
-
-    def parse(self, response):
+    def parse(self, response, params):
         response_json = json.loads(response.body)
         elements = response_json.get("elements")
-        if not elements:
-            self.has_more = False
-            return
         for element in elements:
             bookmaker = Bookmaker()
             bookmaker["external_id"] = element.get("id")
             bookmaker["name"] = element.get("name")
             yield bookmaker
+
+        next_offset = response_json.get("offset") + response_json.get("limit")
+        has_more = response_json.get("count") > next_offset
+        if has_more:
+            params["offset"] = next_offset
+            url = self.build_url_from_params(params)
+            yield response.follow(url, callback=self.parse, cb_kwargs={"params": params})
+
+    @staticmethod
+    def build_url_from_params(params):
+        params_encoded = urllib.parse.urlencode(params)
+        url = "https://metaratings.ru/api/" + "?" + params_encoded
+        return url
