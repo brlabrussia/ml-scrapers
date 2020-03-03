9c60d2d63a880b9427386e1de2e02cf369147808
Valerii Ryndin <ryndin18@icloud.com>
2020-03-03 15:43:31 +0300

    kushvsporte.ru spider - fixes to make more in accordance with current items and pipelines

--- scrapy_project/spiders/kushvsporte.py
+++ scrapy_project/spiders/kushvsporte.py
@@ -8,6 +8,7 @@ from scrapy_project.items import ReviewLoader
 
 class KushvsporteSpider(scrapy.Spider):
     name = "kushvsporte"
+    source_name = "Kush v sporte"
     allowed_domains = ["kushvsporte.ru"]
 
     def start_requests(self):
@@ -15,26 +16,33 @@ class KushvsporteSpider(scrapy.Spider):
         yield scrapy.Request(url, callback=self.parse_bookmakers)
 
     def parse_bookmakers(self, response):
-        xp = "//div[has-class('blockBkList')]//a[@class='medium']/@href"
-        bookmaker_links = response.xpath(xp).getall()
-        for bookmaker_link in bookmaker_links:
-            yield response.follow(bookmaker_link, callback=self.parse_reviews)
+        xp = "//div[has-class('blockBkList')]/*"
+        bookmaker_blocks = response.xpath(xp)
+        for bookmaker_block in bookmaker_blocks:
+            bookmaker_name = bookmaker_block.xpath(".//h3/text()").get()
+            bookmaker_link = bookmaker_block.xpath(".//a[@class='medium']/@href").get()
+            cb_kwargs = {"bookmaker_name": bookmaker_name}
+            yield response.follow(bookmaker_link, callback=self.parse_reviews, cb_kwargs=cb_kwargs)
 
-    def parse_reviews(self, response):
+    def parse_reviews(self, response, bookmaker_name):
         xp = "//*[@id='reviews-block']/div[has-class('review')]"
         reviews = response.xpath(xp)
         for review in reviews:
             loader = ReviewLoader(selector=review)
 
-            # TODO: format date, build content (use pipelines or build in django), empty fields handling
-            loader.add_xpath("create_dtime", ".//meta[@itemprop='datePublished']/@datetime")
-            loader.add_xpath("rating", ".//meta[@itemprop='ratingValue']/@content")
+            # TODO: format date
+            loader.add_value("bookmaker", bookmaker_name)
+            loader.add_value("source", self.source_name)
+
+            loader.add_value("content", "")
+            loader.add_value("title", "")
+            loader.add_xpath("comment", ".//div[@itemprop='reviewBody']//p[3]/text()")
             loader.add_xpath("pluses", ".//div[@itemprop='reviewBody']//p[1]/text()")
             loader.add_xpath("minuses", ".//div[@itemprop='reviewBody']//p[2]/text()")
-            loader.add_xpath("comment", ".//div[@itemprop='reviewBody']//p[3]/text()")
+
+            loader.add_xpath("rating", ".//meta[@itemprop='ratingValue']/@content")
             loader.add_xpath("username", ".//div[has-class('infoUserRevievBK')]/*[@itemprop='name']/@title")
-            loader.add_value("title", "")
-            loader.add_value("content", "")
+            loader.add_xpath("create_dtime", ".//meta[@itemprop='datePublished']/@datetime")
 
             yield loader.load_item()
 
@@ -42,4 +50,5 @@ class KushvsporteSpider(scrapy.Spider):
         next_page = response.xpath(xp).get()
         if next_page:
             next_page = json.loads(next_page)[0]
-            yield response.follow(next_page, callback=self.parse_reviews)
+            cb_kwargs = {"bookmaker_name": bookmaker_name}
+            yield response.follow(next_page, callback=self.parse_reviews, cb_kwargs=cb_kwargs)
