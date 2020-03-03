cc6ee0917c5a5f2c1103a0cb9b47744d3c01d0b3
Valerii Ryndin <ryndin18@icloud.com>
2020-03-03 18:58:44 +0300

    Better BuildContentPipeline

--- scrapy_project/pipelines.py
+++ scrapy_project/pipelines.py
@@ -12,17 +12,17 @@ class NormalizeValuesPipeline(object):
 class BuildContentPipeline(object):
     def process_item(self, item, spider):
         if not item.get("content"):
-            title = item.get("title")
-            title_term = f'"Заголовок": {title}' if title else ""
-            pluses = item.get("pluses")
-            pluses_term = f'"Плюсы": {pluses}' if pluses else ""
-            minuses = item.get("minuses")
-            minuses_term = f'"Минусы": {minuses}' if minuses else ""
-            comment = item.get("comment")
-            comment_term = f'"Комментарий": {comment}' if comment else ""
-            content = title_term + "\n\n" + pluses_term + "\n\n" + minuses_term + "\n\n" + comment_term
+            content = ""
+            content += self.format_value(item.get("title"), "Заголовок")
+            content += self.format_value(item.get("pluses"), "Плюсы")
+            content += self.format_value(item.get("minuses"), "Минусы")
+            content += self.format_value(item.get("comment"), "Комментарий")
             content = content.strip()
             if not content:
                 raise DropItem(f"Can't build content for {item}")
             item["content"] = content
         return item
+
+    @staticmethod
+    def format_value(value, name):
+        return f'"{name}": {value}\n\n' if value else ""
