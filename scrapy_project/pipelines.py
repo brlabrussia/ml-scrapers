90252ebd1b6f27787eadfc4037a6542cd4b04ec9
Valerii Ryndin <ryndin20@yandex.ru>
2020-03-05 21:12:13 +0000

    BuildContentPipeline - refactoring

--- scrapy_project/pipelines.py
+++ scrapy_project/pipelines.py
@@ -1,21 +1,27 @@
-import dateparser
 from scrapy.exceptions import DropItem
 
 
 class BuildContentPipeline(object):
+    """
+    Try to build `content` field from secondary fields: (`title`, `comment`,
+    `pluses` and `minuses`), pop them in the process.
+    Drop item on failure.
+    """
+
     def process_item(self, item, spider):
+        def decorate_value(value, prefix):
+            return f'"{prefix}": {value}\n\n' if value else ""
+
         if not item.get("content"):
-            content = ""
-            content += self.format_value(item.pop("title", None), "Заголовок")
-            content += self.format_value(item.pop("pluses", None), "Плюсы")
-            content += self.format_value(item.pop("minuses", None), "Минусы")
-            content += self.format_value(item.pop("comment", None), "Комментарий")
+            value_prefix_pairs = [
+                (item.pop("title", None), "Заголовок"),
+                (item.pop("pluses", None), "Плюсы"),
+                (item.pop("minuses", None), "Минусы"),
+                (item.pop("comment", None), "Комментарий"),
+            ]
+            content = "".join(decorate_value(v, p) for v, p in value_prefix_pairs)
             content = content.strip()
             if not content:
                 raise DropItem(f"Can't build content for {item}")
             item["content"] = content
         return item
-
-    @staticmethod
-    def format_value(value, name):
-        return f'"{name}": {value}\n\n' if value else ""
