c85dc8cf5245dda65b56142f6e1a1a994c8e1886
Valerii Ryndin <ryndin18@icloud.com>
2020-03-06 08:14:11 +0000

    Minor Scrapy project changes

--- scrapy_project/settings.py
+++ scrapy_project/settings.py
@@ -5,8 +5,12 @@ NEWSPIDER_MODULE = "scrapy_project.spiders"
 
 ROBOTSTXT_OBEY = False
 
+CONCURRENT_REQUESTS = 16
+
 ITEM_PIPELINES = {
     "scrapy_project.pipelines.BuildContentPipeline": 1,
 }
 
+FEED_URI = "export"
+FEED_FORMAT = "jsonlines"
 FEED_EXPORT_ENCODING = "utf-8"
