6c3bb17da0924db06a2082b70f59fa6b9151f4a6
Valerii Ryndin <ryndin20@yandex.ru>
2020-03-31 17:01:45 +0000

    Remove postgres

--- sentiment_ru/pipelines.py
+++ sentiment_ru/pipelines.py
@@ -2,7 +2,6 @@ import hashlib
 import json
 import os
 
-import psycopg2
 from scrapy.exceptions import CloseSpider, DropItem
 
 
@@ -29,52 +28,4 @@ class BuildContentPipeline(object):
             if not content:
                 raise DropItem("Can't build content")
             item["content"] = content
-        return item
-
-
-class PostgresPipeline(object):
-    """
-    Exports items to database `scrapy`, table `<spider-name>`,
-    schema `id CHAR (32) PRIMARY KEY, item JSON`.
-    Checks if item is already in said table.
-    """
-
-    def open_spider(self, spider):
-        # Basic setup and creation of table if it doesn't already exist
-        try:
-            self.connection = psycopg2.connect(host="postgres", user="postgres", password=os.environ.get("PGPASSWORD"), dbname="scrapy")
-        except psycopg2.OperationalError:
-            raise CloseSpider("Can't connect to Postgres")
-        self.cursor = self.connection.cursor()
-        self.cursor.execute(f"CREATE TABLE IF NOT EXISTS {spider.name} (id CHAR (32) PRIMARY KEY, item JSON);")
-        self.connection.commit()
-
-        # Get ids for items we already have in db
-        self.cursor.execute(f"SELECT id FROM {spider.name};")
-        self.ids_seen = set(row[0] for row in self.cursor.fetchall())
-
-    def close_spider(self, spider):
-        self.cursor.close()
-        self.connection.close()
-
-    def process_item(self, item, spider):
-        if item.get("content") is not None:  # Review items
-            id_ = item.get("bookmaker") + item.get("username") + item.get("content")
-        elif item.get("id") is not None:  # Bookmaker items
-            id_ = str(item.get("id"))
-
-        id_ = hashlib.md5(id_.encode()).hexdigest()
-
-        if id_ in self.ids_seen:
-            raise DropItem("Item already in database")
-        else:
-            item_ = json.dumps(dict(item), ensure_ascii=False)
-            try:
-                self.cursor.execute(f"INSERT INTO {spider.name} VALUES (%s, %s);", (id_, item_))
-                self.connection.commit()
-            except psycopg2.Error as error:
-                self.connection.rollback()
-                spider.logger.warning(error)
-                raise DropItem("Item insertion failed")
-
         return item
--- sentiment_ru/settings.py
+++ sentiment_ru/settings.py
@@ -11,11 +11,9 @@ TELNETCONSOLE_ENABLED = False
 
 ITEM_PIPELINES = {
     "sentiment_ru.pipelines.BuildContentPipeline": 1,
-    "sentiment_ru.pipelines.PostgresPipeline": 200,
 }
 
-# FEED_URI = "export"
-# FEED_FORMAT = "jsonlines"
-# FEED_EXPORT_ENCODING = "utf-8"
+FEED_FORMAT = "jsonlines"
+FEED_EXPORT_ENCODING = "utf-8"
 
 LOG_LEVEL = "INFO"
