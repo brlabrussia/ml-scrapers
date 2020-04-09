0d295392e4d3a81c5f4cb713655c55a691eafd3a
Valerii Ryndin <ryndin20@yandex.ru>
2020-04-09 15:10:46 +0000

    Splash fixes and optimizations

--- sentiment_ru/settings.py
+++ sentiment_ru/settings.py
@@ -1,11 +1,11 @@
-from fake_useragent import UserAgent
+import fake_useragent
 
-BOT_NAME = "sentiment_ru"
+BOT_NAME = 'sentiment_ru'
 
-SPIDER_MODULES = ["sentiment_ru.spiders"]
-NEWSPIDER_MODULE = "sentiment_ru.spiders"
+SPIDER_MODULES = ['sentiment_ru.spiders']
+NEWSPIDER_MODULE = 'sentiment_ru.spiders'
 
-USER_AGENT = UserAgent().chrome
+USER_AGENT = fake_useragent.UserAgent().chrome
 
 ROBOTSTXT_OBEY = False
 
@@ -13,30 +13,32 @@ CONCURRENT_REQUESTS = 16
 
 TELNETCONSOLE_ENABLED = False
 
-SPIDER_MIDDLEWARES = {
-    "scrapy_splash.SplashDeduplicateArgsMiddleware": 100,
-}
+SPIDER_MIDDLEWARES = {}
 
-DOWNLOADER_MIDDLEWARES = {
-    "scrapy_splash.SplashCookiesMiddleware": 723,
-    "scrapy_splash.SplashMiddleware": 725,
-    "scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware": 810,
-}
+DOWNLOADER_MIDDLEWARES = {}
 
 ITEM_PIPELINES = {
-    "sentiment_ru.pipelines.BuildContentPipeline": 1,
-    "sentiment_ru.pipelines.WebhookPipeline": 800,
+    'sentiment_ru.pipelines.BuildContentPipeline': 1,
+    'sentiment_ru.pipelines.WebhookPipeline': 800,
 }
 
-FEED_EXPORT_ENCODING = "utf-8"
+FEED_EXPORT_ENCODING = 'utf-8'
 
-LOG_LEVEL = "INFO"
+LOG_LEVEL = 'INFO'
 
-# Splash-related settings
-SPLASH_URL = "http://splash:8050/"
-DUPEFILTER_CLASS = "scrapy_splash.SplashAwareDupeFilter"
-HTTPCACHE_STORAGE = "scrapy_splash.SplashAwareFSCacheStorage"
+# Splash settings https://github.com/scrapy-plugins/scrapy-splash
+SPLASH_URL = 'http://splash:8050/'
+DOWNLOADER_MIDDLEWARES.update({
+    'scrapy_splash.SplashCookiesMiddleware': 723,
+    'scrapy_splash.SplashMiddleware': 725,
+    'scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware': 810,
+})
+SPIDER_MIDDLEWARES.update({
+    'scrapy_splash.SplashDeduplicateArgsMiddleware': 100,
+})
+DUPEFILTER_CLASS = 'scrapy_splash.SplashAwareDupeFilter'
+HTTPCACHE_STORAGE = 'scrapy_splash.SplashAwareFSCacheStorage'
 
 # Custom settings for WebhookPipeline
-WEBHOOK_URLS = ["https://httpbin.org/post"]
+WEBHOOK_URLS = ['https://httpbin.org/post']
 WEBHOOK_CHUNK_SIZE = 1000
