565f7d92b352db5032a76a4474a53c5ecc3f790c
Valerii Ryndin <ryndin20@yandex.ru>
2020-06-01 04:26:40 +0000

    Added slotcatalog_slots scraper

--- app/casinodb/settings.py
+++ app/casinodb/settings.py
@@ -4,6 +4,8 @@ BOT_NAME = 'casinodb'
 SPIDER_MODULES = ['casinodb.spiders']
 NEWSPIDER_MODULE = 'casinodb.spiders'
 
+NORDVPN_ENABLED = True
+
 HTTPCACHE_ENABLED = True
 HTTPCACHE_DIR = 'httpcache'
 HTTPCACHE_EXPIRATION_SECS = 3600 * 36  # 36 hours
@@ -11,7 +13,6 @@ HTTPCACHE_POLICY = 'casinodb.extensions.CasinoguruCachePolicy'
 HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.DbmCacheStorage'
 
 CASINOGURU_CUSTOM_SETTINGS = {
-    'NORDVPN_ENABLED': True,
     'CONCURRENT_REQUESTS': 1,
     'DOWNLOAD_DELAY': 0.5,
     'AUTOTHROTTLE_ENABLED': True,
