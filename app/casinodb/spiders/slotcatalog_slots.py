c192878303e40981e9829508c6149152762b1e0e
Valerii Ryndin <ryndin20@yandex.ru>
2020-06-04 20:57:39 +0000

    Update slotcatalog_slots

--- app/casinodb/spiders/slotcatalog_slots.py
+++ app/casinodb/spiders/slotcatalog_slots.py
@@ -1,6 +1,7 @@
+import json
+
 import scrapy
 from furl import furl
-from slugify import slugify
 
 
 class SlotcatalogSlotsSpider(scrapy.Spider):
@@ -8,8 +9,9 @@ class SlotcatalogSlotsSpider(scrapy.Spider):
     allowed_domains = ['slotcatalog.com']
 
     def start_requests(self):
+        url = 'https://slotcatalog.com/en/The-Best-Slots'
         yield scrapy.Request(
-            'https://slotcatalog.com/en/The-Best-Slots',
+            furl(url).set({'cISO': 'RU'}).url,
             self.parse_slots_list,
             meta={'dont_cache': True},
         )
@@ -22,38 +24,49 @@ class SlotcatalogSlotsSpider(scrapy.Spider):
             if not slot_url or not slot_name:
                 continue
             yield response.follow(
-                slot_url,
+                furl(slot_url).set({'cISO': 'RU'}).url,
                 self.parse_slot,
                 cb_kwargs={'slot_name': slot_name},
                 meta={'dont_cache': False},
             )
         next_page = response.css('.navpag-cont .active + li a::attr(p)').get()
         if next_page:
-            f = furl(response.request.url)
-            f.args['p'] = next_page
-            yield response.request.replace(url=f.url)
+            url = furl(response.url).set({'cISO': 'RU', 'p': next_page}).url
+            yield response.request.replace(url=url)
 
     def parse_slot(self, response, slot_name):
-        # TODO switch to item loader
+        def extract_props(css_prop_blocks, css_prop_name, css_prop_value):
+            props = {}
+            prop_blocks = response.css(css_prop_blocks)
+            for pb in prop_blocks:
+                prop_name = pb.css(css_prop_name).get()
+                if not prop_name:
+                    continue
+                prop_value = pb.css(css_prop_value).getall()
+                props[prop_name.strip()] = ''.join(prop_value).strip()
+            return props
         item = {
             'source': 'slotcatalog',
             'type': 'slots',
             'name': slot_name,
-            'url': response.request.url,
+            'url': furl(response.url).set({}).url,
+            'props_upper': extract_props('.slot_page_item_one > div', 'p::text', 'span *::text'),
+            'props_lower': extract_props('.slotAttrTable tr', '.propLeft::text', '.propRight *::text'),
             'tags': response.css('.tegs_slot_item a::text').getall(),
-            'provider_url': response.css('.provider_item a::attr(href)').getall(),
-            'layout': response.css('.layout_val::text').getall(),
+            'json': json.loads(response.css('#divSlotPage script[type$=json]::text').get() or '{}'),
         }
-        attr_blocks = response.css('.slotAttrTable tr') or ()
-        for ab in attr_blocks:
-            attr_name = ab.css('.propLeft::text').get()
-            if not attr_name:
-                continue
-            attr_name = slugify(attr_name, separator='_')
-            attr_value = ab.css('.propRight *::text').getall()
-            item[attr_name] = attr_value
-        for key in item:
-            if isinstance(item[key], list):
-                sep = '' if key != 'tags' else ', '
-                item[key] = sep.join(item[key]).strip()
+        slug = furl(response.url).path.segments[-1]
+        query = {
+            'slottranslit': slug,
+            'ajax': 1,
+            'blck': 'gameContainer',
+        }
+        url = furl('https://slotcatalog.com/index.php', query=query).url
+        yield scrapy.Request(url, self.parse_iframe, cb_kwargs={'item': item})
+
+    def parse_iframe(self, response, item):
+        item.update({
+            'iframe_original': response.css('.iframeGame::attr(src)').get(),
+            'iframe_fallback': item.get('url').replace('slots', 'play'),
+        })
         yield item
