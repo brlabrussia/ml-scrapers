1e1abb657b3047db0c808d62b5cec1ae75029c08
Valerii Ryndin <ryndin20@yandex.ru>
2020-06-08 03:59:44 +0000

    slotcatalog_providers

--- app/casinodb/spiders/slotcatalog_providers.py
+++ app/casinodb/spiders/slotcatalog_providers.py
@@ -1,6 +1,5 @@
 import scrapy
 from furl import furl
-from slugify import slugify
 
 
 class SlotcatalogProvidersSpider(scrapy.Spider):
@@ -17,11 +16,9 @@ class SlotcatalogProvidersSpider(scrapy.Spider):
     def parse_countries(self, response):
         countries = response.css('select[name=cISO] option::attr(value)').getall()
         for country in countries:
-            f = furl(response.request.url)
-            f.args['cISO'] = country
             yield scrapy.Request(
-                url=f.url,
-                callback=self.parse_providers_list,
+                furl(response.url).set({'cISO': country}).url,
+                self.parse_providers_list,
                 meta={'dont_cache': True},
             )
 
@@ -32,9 +29,8 @@ class SlotcatalogProvidersSpider(scrapy.Spider):
             provider_name = ci.css('.providerName::text').get()
             if not provider_url or not provider_name:
                 continue
-            provider_url = furl(provider_url).pathstr
             yield scrapy.Request(
-                response.urljoin(provider_url),
+                furl(response.url).join(provider_url).set({'cISO': 'RU'}).url,
                 self.parse_provider,
                 cb_kwargs={'provider_name': provider_name},
                 meta={'dont_cache': False},
@@ -46,25 +42,26 @@ class SlotcatalogProvidersSpider(scrapy.Spider):
             yield response.request.replace(url=f.url)
 
     def parse_provider(self, response, provider_name):
-        # TODO switch to item loader
-        item = {
+        def extract_props(css_prop_blocks, css_prop_name, css_prop_value):
+            props = {}
+            prop_blocks = response.css(css_prop_blocks)
+            for pb in prop_blocks:
+                prop_name = pb.css(css_prop_name).get()
+                if not prop_name:
+                    continue
+                if prop_name.strip().lower().startswith('website'):
+                    css_prop_value_website = css_prop_value.replace('*::text', 'a::attr(href)')
+                    prop_value = pb.css(css_prop_value_website).getall()
+                else:
+                    prop_value = pb.css(css_prop_value).getall()
+                props[prop_name.strip()] = ''.join(prop_value).strip()
+            return props
+        yield {
             'source': 'slotcatalog',
             'type': 'providers',
             'name': provider_name,
-            'url': response.request.url,
-            'countries': response.css('.sixNameLeftCountries[data-label=Country] a::text').getall(),
+            'url': furl(response.url).set({}).url,
+            'props_upper': extract_props('.provider_prop_item_pad', '.prop_name::text', '.prop_number::text'),
+            'props_lower': extract_props('.provDetail', '.provDetailTitle::text', '.provDetailField *::text'),
+            'countries': response.css('.sixNameLeftCountries[data-label=Country] .linkOneLine::text').getall(),
         }
-        attr_blocks = response.css('.provDetail') or ()
-        for ab in attr_blocks:
-            attr_name = ab.css('.provDetailTitle::text').get()
-            if not attr_name:
-                continue
-            attr_name = slugify(attr_name, separator='_')
-            attr_value = ab.css('.provDetailField *::text').getall()
-            if attr_name == 'website':
-                attr_value = ab.css('.provDetailField a::attr(href)').getall()
-            item[attr_name] = attr_value
-        for key in item:
-            if isinstance(item[key], list):
-                item[key] = ''.join(item[key]).strip()
-        yield item
