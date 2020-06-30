74eafc40907c21daa3eca6b051643b0ffef906c3
Valerii Ryndin <ryndin20@yandex.ru>
2020-06-30 03:19:11 +0300

    ScrapyMetaFieldPipeline and SentimentSourceFieldPipeline pipelines

--- app/sentiment_ratings/settings.py
+++ app/sentiment_ratings/settings.py
@@ -6,3 +6,8 @@ SPIDER_MODULES = ['sentiment_ratings.spiders']
 NEWSPIDER_MODULE = 'sentiment_ratings.spiders'
 
 CONCURRENT_REQUESTS = 16
+
+ITEM_PIPELINES.update({
+    'default.pipelines.ScrapyMetaFieldPipeline': 1,
+    'default.pipelines.SentimentSourceFieldPipeline': 2,
+})
