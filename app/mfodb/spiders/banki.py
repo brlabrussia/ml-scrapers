f29526b40ef61da180d0d1bafab1612a21a7a67e
Valerii Ryndin <51380972+ryndin20@users.noreply.github.com>
2020-07-27 08:27:14 +0300

    Mfodb v2 (#14)
    * add items
    
    * banki spider
    
    * cbr spider
    
    * vsezaimyonline spider
    
    * zaymov spider

--- app/mfodb/spiders/banki.py
+++ app/mfodb/spiders/banki.py
@@ -1,5 +1,7 @@
 import scrapy
 
+from mfodb.items import BankiLoader
+
 
 class BankiSpider(scrapy.Spider):
     name = 'banki'
@@ -23,6 +25,53 @@ class BankiSpider(scrapy.Spider):
             yield response.follow(link, self.parse_product)
 
     def parse_product(self, response):
+        # xpath selectors constructor
+        xp = '//*[has-class("definition-list__key")][normalize-space(text())="{}"]/../*[has-class("definition-list__value")]{}'
+        as_text = '/text()'
+        as_list = '/ul/li/text()'
+        as_note = '/*[has-class("text-note")]/p/text()'
+
+        bl = BankiLoader(response=response)
+        bl.add_value('url', response.url)
+        bl.add_css('name', 'h1::text')
+        bl.add_css('logo', '[data-test=mfo-logo]::attr(src)')
+        bl.add_xpath('updated_at', '//*[has-class("text-note")][starts-with(normalize-space(text()), "Дата актуализации")]/text()', re=r'\d{2}\.\d{2}\.\d{4} \d+:\d{2}')
+        # * О займе
+        # ** Условия и ставки
+        bl.add_xpath('loan_purpose', xp.format('Цель займа', as_list))
+        bl.add_xpath('max_money_value', xp.format('Сумма займа', as_text), re=r'до ([\d ]+)')
+        bl.add_xpath('first_loan_condition', xp.format('Сумма займа', as_note))
+        bl.add_xpath('rate', xp.format('Ставка', as_text))  # re=r'([\d\,]+)'
+        bl.add_xpath('dates_from', xp.format('Срок', as_text), re=r'от (\d+)')  # either 'от 1 до 365 дней'
+        bl.add_xpath('dates_from', xp.format('Срок', as_text), re=r'^(\d+) дней')  # or '7 дней'
+        bl.add_xpath('dates_to', xp.format('Срок', as_text), re=r'до (\d+)')  # either 'от 1 до 365 дней'
+        bl.add_xpath('dates_to', xp.format('Срок', as_text), re=r'^(\d+) дней')  # or '7 дней'
+        bl.add_xpath('loan_time_terms', xp.format('Срок', as_note))
+        bl.add_xpath('loan_providing', xp.format('Обеспечение', as_list))
+        # ** Требования и документы
+        bl.add_xpath('borrowers_categories', xp.format('Категория заемщиков', as_list))
+        bl.add_xpath('borrowers_age', xp.format('Возраст заемщика', as_text))
+        bl.add_xpath('borrowers_registration', xp.format('Регистрация', as_list))
+        bl.add_xpath('borrowers_documents', xp.format('Документы', as_list))
+        # ** Выдача
+        bl.add_xpath('issuance', xp.format('Срок выдачи', as_text))
+        bl.add_xpath('loan_processing', xp.format('Оформление займа', as_list))
+        bl.add_xpath('loan_form', xp.format('Форма выдачи', as_list))
+        bl.add_xpath('loan_form_description', xp.format('Форма выдачи', as_note))
+        # ** Погашение
+        bl.add_xpath('repayment_order', xp.format('Порядок погашения', as_list))
+        bl.add_xpath('repayment_order_description', xp.format('Порядок погашения', as_note))
+        bl.add_xpath('payment_methods', xp.format('Способ оплаты', as_list))
+        # * Об организации
+        bl.add_xpath('trademark', xp.format('Торговая марка', as_text))
+        bl.add_xpath('head_name', xp.format('Руководитель', as_text))
+        bl.add_xpath('address', xp.format('Адрес', as_text))
+        bl.add_xpath('ogrn', xp.format('ОГРН', as_text), re=r'\d{5,}')
+        bl.add_xpath('reg_number', xp.format('Рег. номер', as_text), re=r'\d{5,}')
+        bl.add_xpath('website', xp.format('Официальный сайт', '/a/text()'))
+        yield bl.load_item()
+
+    def parse_product_old(self, response):
         item = {
             'subject': response.css('h1::text').get(),
             'url': response.url,
--- app/mfodb/spiders/cbr.py
+++ app/mfodb/spiders/cbr.py
@@ -3,6 +3,8 @@ import io
 import openpyxl
 import scrapy
 
+from mfodb.items import CbrLoader
+
 
 class CbrSpider(scrapy.Spider):
     name = 'cbr'
@@ -15,15 +17,15 @@ class CbrSpider(scrapy.Spider):
     def parse_subjects(self, response):
         workbook = openpyxl.load_workbook(io.BytesIO(response.body))
         for row in workbook.active.iter_rows(min_row=6, values_only=True):
-            yield {
-                'reg_number': ''.join(row[1:6]),
-                'registry_date': row[6],
-                'mfo_type': row[8],
-                'ogrn': row[9],
-                'inn': row[10],
-                'full_name': row[11],
-                'name': row[12],
-                'address': row[13],
-                'url': row[14],
-                'email': row[15],
-            }
+            cl = CbrLoader()
+            cl.add_value('reg_number', ''.join(row[1:6]))
+            cl.add_value('registry_date', row[6])
+            cl.add_value('mfo_type', row[8])
+            cl.add_value('ogrn', row[9])
+            cl.add_value('inn', row[10])
+            cl.add_value('full_name', row[11])
+            cl.add_value('name', row[12])
+            cl.add_value('address', row[13])
+            cl.add_value('url', row[14])
+            cl.add_value('email', row[15])
+            yield cl.load_item()
