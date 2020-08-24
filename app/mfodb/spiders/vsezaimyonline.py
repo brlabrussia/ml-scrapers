54caa91d2a2d1f7fab7a3315b074951a616224d4
Valerii Ryndin <ryndin20@yandex.ru>
2020-08-24 10:23:30 +0300

    vsezaimyonline spider

--- app/mfodb/spiders/vsezaimyonline.py
+++ app/mfodb/spiders/vsezaimyonline.py
@@ -1,4 +1,6 @@
+import requests
 import scrapy
+from scrapy.selector import Selector
 
 from mfodb.items import VsezaimyonlineLoader
 
@@ -23,12 +25,29 @@ class VsezaimyonlineSpider(scrapy.Spider):
                 'url': document_block.css('::attr(href)').get(),
             })
         vl = VsezaimyonlineLoader(response=response)
-        vl.add_value('url', response.url)
-        vl.add_css('name', '.zaym-name::text')
+        vl.add_value('scraped_from', response.url)
+        vl.add_css('trademark', '.zaym-name::text')
         vl.add_css('logo', '.logo-company::attr(src)')
-        vl.add_xpath('ogrn', '//div[has-class("vab")]//li[starts-with(normalize-space(text()), "ОГРН")]/text()', re=r'\d{5,}')
-        vl.add_xpath('inn', '//div[has-class("vab")]//li[starts-with(normalize-space(text()), "ИНН")]/text()', re=r'\d{5,}')
-        vl.add_css('refusal_reasons', 'div[data-id="1"] li::text')
-        vl.add_css('social_networks', 'div[data-id="2"] li a::attr(href)')
+        vl.add_xpath('ogrn', '//div[has-class("vab")]//li[starts-with(normalize-space(text()), "ОГРН")]/text()', re=r'\d{13}')
+        vl.add_xpath('inn', '//div[has-class("vab")]//li[starts-with(normalize-space(text()), "ИНН")]/text()', re=r'\d{10}')
+        decline_reasons_id = response.xpath('//*[@id="single_content_wrap"]//li[normalize-space(text())="Причины отказа"]/@data-id').get()
+        if decline_reasons_id:
+            vl.add_css('decline_reasons', f'#single_content_wrap .right-block div[data-id="{decline_reasons_id}"] *::text')
+        socials_id = response.xpath('//*[@id="single_content_wrap"]//li[normalize-space(text())="Служба поддержки"]/@data-id').get()
+        if socials_id:
+            vl.add_css('socials', f'#single_content_wrap .right-block div[data-id="{socials_id}"] li a::attr(href)')
         vl.add_value('documents', documents)
+
+        # `decision_speed` and `payment_speed` are loaded separately
+        company_id = response.css('script').re_first('window.company_id = (\d+)')
+        if company_id:
+            url = f'https://vsezaimyonline.ru/actions/load_card_for_company?company_id={company_id}'
+            r = requests.get(url)
+            selector = Selector(text=r.json().get('code', ''))
+            xp = '//div[starts-with(normalize-space(text()), "{}")]/div/text()'
+            decision_speed = selector.xpath(xp.format('Скорость рассмотрения заявки')).get()
+            payment_speed = selector.xpath(xp.format('Скорость выплаты')).get()
+            vl.add_value('decision_speed', decision_speed)
+            vl.add_value('payment_speed', payment_speed)
+
         yield vl.load_item()
