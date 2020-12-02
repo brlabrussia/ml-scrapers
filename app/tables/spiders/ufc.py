ba0f2a2a2f18748c8cbc028e842b2a1aae67827a
Valerii Ryndin <ryndin20@yandex.ru>
2020-12-02 17:35:31 +0300

    tables ufc spider beta

--- app/tables/spiders/ufc.py
+++ app/tables/spiders/ufc.py
@@ -1,6 +1,18 @@
-import scrapy
+import re
 
-from tables.items import TableDataLoader, TableLoader
+import scrapy
+from scrapy.loader.processors import Compose
+
+from tables.items_ import TableDataLoader, TableLoader
+from tables.utils import (
+    normalize_style_attributes,
+    prepare_table_selector,
+    remove_browser_css_properties,
+    remove_color_css_properties,
+    remove_style_tags,
+    remove_weird_css_properties,
+    replace_anchors,
+)
 
 
 class UfcSpider(scrapy.Spider):
@@ -8,31 +20,37 @@ class UfcSpider(scrapy.Spider):
     allowed_domains = ['bsrussia.com']
     start_urls = ['https://ru.ufc.com/rankings']
     table_name = 'Полусредний вес'
+    post_processor = Compose(
+        remove_style_tags,
+        # remove_unwanted_attributes,
+        normalize_style_attributes,
+        remove_browser_css_properties,
+        # remove_weird_css_properties,
+        remove_color_css_properties,
+        replace_anchors,
+        lambda x: x.replace('visibility:hidden;', ''),
+        lambda x: re.sub(r'border-bottom.*?;', r'', x, flags=re.S),
+        # lambda x: re.sub(r'\s(class|href)="[^"]*?"', r'', x, flags=re.S),
+    )
 
     def parse(self, response):
         tl = TableLoader(response=response)
         tl.add_value('url', response.url)
         tl.add_value('title', self.table_name)
-        # xp = f'//*[@class="view-grouping-header"][normalize-space(text())="{self.table_name}"]/following-sibling::*[@class="view-grouping-content"]'
         xp = f'//*[@class="info"]/h4[normalize-space(text())="{self.table_name}"]/ancestor::table'
-        table_sel = response.xpath(xp)
+        table_sel = prepare_table_selector(
+            response.css('body'),
+            response,
+            post_processor=self.post_processor,
+        )
+        table_sel = table_sel.xpath(xp)
 
         # upper body
         row_loaders = []
         tdl = TableDataLoader(selector=table_sel)
-        tdl.base_url = response.url
-        tdl.add_xpath('value', './/*[@class="info"]/h6/node()')
-        tdl.add_xpath('value', './/*[@class="info"]/h4/span/node()')
+        tdl.add_xpath('value', './/caption/node()')
         tdl.add_value('value', '')
-        row_loaders.append(tdl)
-        tdl = TableDataLoader(selector=table_sel)
-        tdl.base_url = response.url
-        tdl.add_xpath('value', './/*[@class="info"]/h5/node()')
-        tdl.add_value('value', '')
-        row_loaders.append(tdl)
-        tdl = TableDataLoader(selector=table_sel)
-        tdl.base_url = response.url
-        tdl.add_value('value', '')
+        tdl.add_value('colspan', '3')
         row_loaders.append(tdl)
         tl.add_value('body', [row_loaders])
 
@@ -41,7 +59,6 @@ class UfcSpider(scrapy.Spider):
             row_loaders = []
             for data_sel in row_sel.css('td'):
                 tdl = TableDataLoader(selector=data_sel)
-                tdl.base_url = response.url
                 tdl.add_xpath('value', './node()')
                 tdl.add_value('value', '')
                 tdl.add_xpath('colspan', './@colspan')
